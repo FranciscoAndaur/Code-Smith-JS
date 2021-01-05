7972005e1b31352a7f5b505eb1e180b1c684564b
FranciscoAndaur <francisco.Andaur@icloud.com>
2021-01-04 20:54:21 -0500

    descriptions added

--- LoopsMulitpleConditions.js
+++ LoopsMulitpleConditions.js
@@ -1,3 +1,6 @@
+// Write a function addingAllTheWeirdStuff which adds the sum of all the odd numbers in array2 to each element under 10 in array1. Similarly, addingAllTheWeirdStuff should also add the sum of all the even numbers in array2 to those elements over 10 in array1.
+// BONUS: If any element in array2 is greater than 20, add 1 to every element in array1.
+
 function addingAllTheWeirdStuff(array1, array2){
     let sumOdds = 0;
     let sumEvens = 0;
